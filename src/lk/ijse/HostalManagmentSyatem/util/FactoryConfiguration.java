cc2abd4beef7d8558062f85758a17d3377afec2b
amilaravinga <amilaravinga@gmail.com>
2023-04-18 12:57:53 +0530

    factory configuration

--- src/lk/ijse/HostalManagmentSyatem/util/FactoryConfiguration.java
+++ src/lk/ijse/HostalManagmentSyatem/util/FactoryConfiguration.java
@@ -1,5 +1,26 @@
 package lk.ijse.HostalManagmentSyatem.util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfiguration(){
+        Configuration configuration = new Configuration();
+        sessionFactory = configuration.buildSessionFactory();
+
+    }
+
+    public static FactoryConfiguration getInstance(){
+        return (factoryConfiguration==null) ? factoryConfiguration=new FactoryConfiguration() : factoryConfiguration ;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+
 
 }
